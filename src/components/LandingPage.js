21ee8f57c238860b7cc433f05a88500240d86694
Youssef Moukhlis <ymoukhli@e1r8p13.1337.ma>
2021-09-13 19:43:39 +0100

    init

---
+++ src/components/LandingPage.js
@@ -0,0 +1,9 @@
+export default function LandingPage() {
+    return (<div>
+        <nav class="w-full fixed from-black">
+            <button class="p-2 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...">Login</button>
+        </nav>
+        <img class="min-h-screen min-w-full object-cover" src="backgroundImage.png"></img>
+        
+    </div>)
+}
